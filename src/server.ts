b4e4f53aff691003827efe50f2defc5d51fe64c5
Arthur Kellermann <arthur.kellermann956@gmail.com>
2023-08-29 15:34:42 -0300

    feat: add dockerfile and docker-compose

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,13 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
 
 const app = express();
+const port = 3333;
 
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('server is running');
 });
