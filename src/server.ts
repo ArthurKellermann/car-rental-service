f3c02b06f4570daf79c20e002a0aeb43488e147d
Arthur Kellermann <arthur.kellermann956@gmail.com>
2023-08-31 16:09:49 -0300

    feat: create category migration with prisma

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import router from './application/routes';
+import router from './routes/index';
 import swaggerUi from 'swagger-ui-express';
 
 import swaggerFile from './swagger.json';
@@ -13,5 +13,5 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(port, () => {
-  console.log('server is running');
+  console.log('Server is running');
 });
