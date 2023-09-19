27205c30c3dc5b82536bb77ff5fa894d073c6f12
Arthur Kellermann <arthur.kellermann956@gmail.com>
2023-09-19 15:48:34 -0300

    feat: add user admin

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,15 +2,18 @@ import { PrismaCategoriesRepository } from './prisma/repositories/prisma-categor
 import { PrismaSpecificationsRepository } from './prisma/repositories/prisma-specifications-repository';
 import { prismaClient } from './prisma/prisma-client';
 import { PrismaUsersRepository } from './prisma/repositories/prisma-users-repository';
+import { PrismaCarsRepository } from './prisma/repositories/prisma-cars-repository';
 
 const prismaCategoriesRepository = new PrismaCategoriesRepository(prismaClient);
 const prismaSpecificationsRepository = new PrismaSpecificationsRepository(
   prismaClient,
 );
 const prismaUsersRepository = new PrismaUsersRepository(prismaClient);
+const prismaCarsRepository = new PrismaCarsRepository(prismaClient);
 
 export {
   prismaCategoriesRepository,
   prismaSpecificationsRepository,
   prismaUsersRepository,
+  prismaCarsRepository,
 };
---
+++ src/database/seed/admin.ts
@@ -0,0 +1,26 @@
+import { hash } from 'bcryptjs';
+import { prismaClient } from '../prisma/prisma-client';
+import { v4 as randomUUID } from 'uuid';
+
+createAdmin().then(() => console.log('User admin created!'));
+
+async function createAdmin() {
+  const id = randomUUID();
+  const password = await hash('admin', 8);
+  try {
+    await prismaClient.user.create({
+      data: {
+        id: id,
+        name: 'Admin',
+        username: 'admin',
+        email: 'admin@rentx.com.br',
+        password: password,
+        isAdmin: true,
+        created_at: new Date(),
+        driver_license: 'xxxxxx',
+      },
+    });
+  } finally {
+    await prismaClient.$disconnect();
+  }
+}
