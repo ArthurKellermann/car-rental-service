763327e18155ac1cfdd9f6b587eccdeef5899ccb
Arthur Kellermann <arthur.kellermann956@gmail.com>
2023-09-23 16:48:22 -0300

    test: add unit test for rentals creation

--- src/modules/rentals/use-cases/create-rental/create-rental-use-case.spec.ts
+++ src/modules/rentals/use-cases/create-rental/create-rental-use-case.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../../../shared/infra/errors/app-error';
 import { InMemoryRentalsRepository } from '../../repositories/in-memory/in-memory-rentals-repository';
 import { CreateRentalUseCase } from './create-rental-use-case';
 
@@ -17,9 +18,39 @@ describe('Create rental', () => {
       expected_return_date: new Date(),
     });
 
-    console.log(rental);
-
     expect(rental).toHaveProperty('id');
     expect(rental).toHaveProperty('start_date');
+  });
+
+  it('should not be able to create a new rental if there is another open to the same user', async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: '12345',
+        car_id: '12312',
+        expected_return_date: new Date(),
+      });
+
+      await createRentalUseCase.execute({
+        user_id: '12345',
+        car_id: '123112',
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create a new rental if there is another open to the same car', async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: '123',
+        car_id: 'test',
+        expected_return_date: new Date(),
+      });
+
+      await createRentalUseCase.execute({
+        user_id: '321',
+        car_id: 'test',
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
